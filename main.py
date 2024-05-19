142c01dc0971bfedc555c2841d0a25ff20939df7
yug1303 <149922985+yug1303@users.noreply.github.com>
2024-05-20 00:34:13 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from sklearn import datasets, linear_model
-from sklearn.metrics import mean_squared_error
-# keys='data', 'target', 'frame', 'DESCR', 'feature_names', 'data_filename', 'target_filename', 'data_module'
-diabetes = datasets.load_diabetes()
-# print(diabetes.data)
-diabetes_x = diabetes.data
-# print(diabetes_x)
-diabetes_x_train = diabetes_x[:-30]
-diabetes_x_test = diabetes_x[-30:]
-diabetes_y_train = diabetes.target[:-30]
-diabetes_y_test = diabetes.target[-30:]
-model = linear_model.LinearRegression()
-model.fit(diabetes_x_train,diabetes_y_train)
-diabetes_y_predict = model.predict(diabetes_x_test)
-print("Mean squared error", mean_squared_error(diabetes_y_test,diabetes_y_predict))
-print("weights", model.coef_)
-print("intercept ", model.intercept_)
-# plt.scatter(diabetes_x_test,diabetes_y_test)
-# # plt.show()
-#
-# plt.plot(diabetes_x_test,diabetes_y_predict)
-# plt.show()
 
+from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+#loading datasets
+iris =datasets.load_iris()
+#printing features
+print(iris.DESCR)
+features = iris.data
+labels = iris.target
+print(features[0],labels[0])
+#training the classifier
+clf = KNeighborsClassifier()
+clf.fit(features,labels)
+preds  = clf.predict([[1,1,1,1]])
+print(preds)
 
